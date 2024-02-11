cad4faf4c2a3051ad5abaff42113552766d3e879
Victor Emanuel <victorers2@gmail.com>
2024-02-11 15:37:00 -0300

    adds footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+/// Strings
+export const SITE_TITLE = 'Victor Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website';
+
+/// URLs
+export const GITHUB_PROFILE_URL = 'https://github.com/victorers1';
+export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/victor-emanuel-809b7a196';
