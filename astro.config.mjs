cb88604f5e15c9fab393630318ae2fedc9f910a0
Victor Emanuel <victorers2@gmail.com>
2024-03-12 18:09:38 -0300

    adds config to MD TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,22 +9,23 @@ import rehypeToc from 'rehype-toc';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), icon(), mdx(
-
-  )],
+  integrations: [sitemap(), icon(), mdx()],
   markdown: {
     remarkPlugins: [
-      remarkMath
+      remarkMath,
+      [remarkToc, {ordered: true}]
     ],
     rehypePlugins: [
-      [rehypeAutolinkHeadings, { behavior: 'append' }],
-      [rehypeToc, { headings: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
       rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'prepend' }],
+      [rehypeToc, { headings: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
       rehypeKatex,
     ],
+
   }
 });
